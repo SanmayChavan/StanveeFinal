d7f77de03e62b8123765ab7c8b39eb9d2679715e
Sanmay Chavan <sanmaychavan22@gmail.com>
2026-01-21 11:40:40 +0530

    server stanveeversel first then localhost

--- server/server.js
+++ server/server.js
@@ -19,8 +19,8 @@ const port = process.env.PORT || 4000;
 
 //Allow multiple origins
 const allowedOrigins = [
-    'http://localhost:5173',
     'https://stanveeproducts.vercel.app',
+    'http://localhost:5173',
 
 ];
 // 'https://greencart-flax.vercel.app'
